a8b15746b290419ac2eaa231f1e69b4f8e8e775e
Kate Arapova <katearapova@gmail.com>
2016-10-22 13:58:27 +0300

    login&regist
    login form popup & userinfo popup

---
+++ js/my-app.js
@@ -0,0 +1,57 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+// myApp.loginScreen();
+// myApp.popup('.popup-userinfo');
+
+var inputL = 0;
+$('input#phone').on('keyup keydown change', function () { 
+    var curchr = this.value.length;
+    var curval = $(this).val();
+    if(inputL < this.value.length) {
+        if (curchr == 3 && curval.indexOf('(') == -1) {
+            $("input#phone").val("(" + curval + ")" + "-");
+        } else if (curchr == 4 && curval.indexOf(')') == -1){
+            $("input#phone").val(curval + ")" + "-");
+        } else if (curchr == 9) {
+            $("input#phone").val(curval + "-");
+        } else if (curchr == 12) {
+            $("input#phone").val(curval + "-");
+        }
+        inputL = this.value.length;
+    } else {
+        inputL = this.value.length;
+    }
+    if($('input#phone').val().length = 14) $('#get-code-btn').removeClass('disabled'); else $('#get-code-btn').addClass('disabled');
+});
+
+$('#get-code-btn').on('click', function(){
+    $('.item-phone, #login-btns').hide();
+    $('.login-sub-title').html('Код полученный в СМС');
+    $('.item-code, #code-btns').show();
+});
+$('#get-password').on('click', function () {
+  var popupHTML = '<div class="popup"><div class="login-screen-title">Введите пароль</div><div class="list-block"><ul><li><div class="item-content"><div class="item-inner"><div class="item-input"><input type="password" placeholder=""></div></div></div></li></ul></div>';
+  myApp.popup(popupHTML);
+});  
+$('#get-password-2').on('click', function () {
+  var popupHTML = '<div class="popup"><div class="login-screen-title">Создайте пароль</div><div class="list-block"><ul><li><div class="item-content"><div class="item-inner"><div class="item-input"><input type="password" placeholder=""></div></div></div></li></ul></div>';
+  myApp.popup(popupHTML);
+});  
+$('#get-password-3').on('click', function () {
+  var popupHTML = '<div class="popup"><div class="login-screen-title">Подтвердите пароль</div><div class="list-block"><ul><li><div class="item-content"><div class="item-inner"><div class="item-input"><input type="password" placeholder=""></div></div></div></li></ul></div>';
+  myApp.popup(popupHTML);
+});  
+$('#regist-finish').on('click', function () {
+  var popupHTML = '<div class="popup"><div class="login-screen-title">Регистрация завершена успешно!</div><a class="button-custom">Начать использование</a></div>';
+  myApp.popup(popupHTML);
+});  
