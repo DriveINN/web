3f48f4b18ae8d87d72b6e50b0358f41d79cef60c
Kate Arapova <katearapova@gmail.com>
2016-10-22 18:02:58 +0300

    Пополнена успешно

--- js/my-app.js
+++ js/my-app.js
@@ -58,3 +58,7 @@ $('#regist-finish').on('click', function () {
   var popupHTML = '<div class="popup"><div class="login-screen-title">Регистрация завершена успешно!</div><a class="button-custom">Начать использование</a></div>';
   myApp.popup(popupHTML);
 });  
+$('#success-buy').on('click', function () {
+  var popupHTML = '<div class="popup"><div class="login-screen-title">Топливная карта<br> успешно пополнена</div><div class="title-custom"><span>+200</span> <i class="icon icon-fire icon-fire-blue"></i> АИ-95</div><div class="label">Пригласи друга и получите по 5 литров топлива каждый!<a>Выбрать друга</a></div><a class="button-custom">Закрыть</a></div>';
+  myApp.popup(popupHTML);
+});  
